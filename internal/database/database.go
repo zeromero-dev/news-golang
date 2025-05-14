b16d5c2ce47cdc1be1e9ba58517afdfb1738f6a5
Roman Holubenko <spiderr.roma@gmail.com>
2025-05-14 20:13:31 +0300

    Initial commit

---
+++ internal/database/database.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/joho/godotenv/autoload"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Service interface {
+	Health() map[string]string
+}
+
+type service struct {
+	db *mongo.Client
+}
+
+var (
+	host = os.Getenv("BLUEPRINT_DB_HOST")
+	port = os.Getenv("BLUEPRINT_DB_PORT")
+	//database = os.Getenv("BLUEPRINT_DB_DATABASE")
+)
+
+func New() Service {
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s", host, port)))
+
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	return &service{
+		db: client,
+	}
+}
+
+func (s *service) Health() map[string]string {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	err := s.db.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("db down: %v", err)
+	}
+
+	return map[string]string{
+		"message": "It's healthy",
+	}
+}
---
+++ internal/server/routes.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
+	"github.com/a-h/templ"
+	"io/fs"
+	"test-news/cmd/web"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // Add your frontend URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type"},
+		AllowCredentials: true, // Enable cookies/auth
+	}))
+
+	r.GET("/", s.HelloWorldHandler)
+
+	r.GET("/health", s.healthHandler)
+
+	staticFiles, _ := fs.Sub(web.Files, "assets")
+	r.StaticFS("/assets", http.FS(staticFiles))
+
+	r.GET("/web", func(c *gin.Context) {
+		templ.Handler(web.HelloForm()).ServeHTTP(c.Writer, c.Request)
+	})
+
+	r.POST("/hello", func(c *gin.Context) {
+		web.HelloWebHandler(c.Writer, c.Request)
+	})
+
+	return r
+}
+
+func (s *Server) HelloWorldHandler(c *gin.Context) {
+	resp := make(map[string]string)
+	resp["message"] = "Hello World"
+
+	c.JSON(http.StatusOK, resp)
+}
+
+func (s *Server) healthHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, s.db.Health())
+}
